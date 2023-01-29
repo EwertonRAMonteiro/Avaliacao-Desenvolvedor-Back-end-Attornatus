f998eec59517cf7f4049ba8d031827d4b2f1f467
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-01-29 16:05:11 -0300

    metodo listagem de endereços por pessoas finalizado

--- src/main/java/com/Attornatus/desafio/repositories/EnderecoRepository.java
+++ src/main/java/com/Attornatus/desafio/repositories/EnderecoRepository.java
@@ -1,10 +1,7 @@
 package com.Attornatus.desafio.repositories;
 
-import com.Attornatus.desafio.dto.EnderecoRequest;
 import com.Attornatus.desafio.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
